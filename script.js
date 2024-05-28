f7b0322e1041ae17f85b3074e916466d5e602d30
“kishlaychandan00” <kishlaychandan00@email.com>
2024-05-28 15:35:55 +0530

    Adding Age Calculator

---
+++ script.js
@@ -0,0 +1,71 @@
+let main=document.getElementsByClassName("main")[0];
+let dateinput=document.querySelector(".dateinput");
+let btn=document.querySelector(".btn");
+let year=document.querySelector(".year");
+let month=document.querySelector(".month");
+let day=document.querySelector(".day");
+let section2=document.querySelector(".section2");
+
+// console.log(day.textContent);
+// console.log(main,dateinput,btn,year,day.innerText,month);
+
+let date=new Date();
+
+btn.addEventListener("click",()=>{
+    // console.log(dateinput.value); 
+    dob=dateinput.value;
+    if (!dob) {
+        alert("Please enter a date of birth");
+        return;
+    }
+    
+    let curdd=date.getDate();
+    let curmm=date.getMonth()+1;
+    let curyyyy=date.getFullYear();
+
+    let dobyyyy=parseInt(dob.slice(0,4));
+    let dobmm=parseInt(dob.slice(5,7));
+    let dobdd=parseInt(dob.slice(8));
+    // console.log(dob,curdd,dobdd);
+
+    let totalyyyy=curyyyy-dobyyyy;
+    let totalmm=curmm-dobmm;
+    let totaldd=curdd-dobdd;
+    if(totaldd<0){
+        totalmm--;
+        let tempDate = new Date(curyyyy, curmm - 1, 0);
+        console.log("date...",tempDate);
+        totaldd += tempDate.getDate();
+    }
+    if(totalmm<0){
+        totalyyyy--;
+        totalmm += 12;
+    }
+    if (totalyyyy < 0 || (totalyyyy === 0 && totalmm < 0) || (totalyyyy === 0 && totalmm === 0 && totaldd < 0)) {
+        alert("Invalid date of birth");
+        year.innerText="...";
+        month.innerText="...";
+        day.innerText="...";
+        section2.style.color="red";
+        return;
+    } 
+    else{
+        console.log(totaldd,totalmm,totalyyyy);
+        year.innerText=totalyyyy;
+        month.innerText=totalmm;
+        day.innerText=totaldd;
+        console.log(year.innerText);
+        section2.style.color="green";
+    }
+    
+});
+let colors=["#fedcba","#ffbf00","#0000ff","#3f3fff","#abcdef"
+    ,"#181716","#009c4a","#ff0000","#451800","#cbff00","#849974","#cbff00","#c0ff00",
+    "#c6e2ff","#ff00ff","#fbc6da"
+];
+setInterval(changebackground,1000);
+function changebackground(){
+    let random=colors[Math.floor(Math.random()*colors.length)];
+    main.style.backgroundColor=random;
+    main.style.transition="all 0.4s linear"
+}
